6beff31821c90339c31a9d4a1c2e53bbe33b4fdb
zhouzhihui <zhouzhihui@htgames.cn>
2017-10-12 14:30:16 +0800

    add a recyclerview in mainactivity.java

--- CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/MainActivity.kt
+++ CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/MainActivity.kt
@@ -6,10 +6,15 @@ import android.support.design.widget.NavigationView
 import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
-import android.view.Menu
-import android.view.MenuItem
+import android.support.v7.widget.CardView
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.*
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
+        //
+        list.setLayoutManager(LinearLayoutManager(this))
+        list.adapter = CardAdapter()
     }
 
     override fun onBackPressed() {
@@ -59,4 +67,41 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
+}
+
+internal class CardAdapter : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(inflater.inflate(R.layout.row_empty_card, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return ITEM_COUNT
+    }
+
+    internal class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var card_view: CardView
+        var tv_position: TextView
+
+        init {
+            card_view = itemView.findViewById<View>(R.id.card_view) as CardView
+            card_view.isClickable = true
+            tv_position = itemView.findViewById<View>(R.id.tv_position) as TextView
+        }
+
+        fun bind(position: Int) {
+            tv_position.text = "$position"
+            card_view.setOnClickListener { v -> Toast.makeText(v.context, "点击: " + position, Toast.LENGTH_SHORT).show() }
+        }
+    }
+
+    companion object {
+        val ITEM_COUNT = 64
+    }
+
 }
