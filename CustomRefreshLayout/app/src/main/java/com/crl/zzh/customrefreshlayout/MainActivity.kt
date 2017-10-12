bc5f44e77ef01db02c3b34e2678a1ae47a7cb874
zhouzhihui <zhouzhihui@htgames.cn>
2017-10-12 11:45:44 +0800

    complete drawerfragment function

--- CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/MainActivity.kt
+++ CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/MainActivity.kt
@@ -2,11 +2,61 @@ package com.crl.zzh.customrefreshlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener {
+            Snackbar.make(it, "Replace with your action", Snackbar.LENGTH_SHORT).setAction("Action", null).show()
+        }
+        var toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        if (id == R.id.menu_setting) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.nav_camera -> {}
+            R.id.nav_gallery -> {}
+            R.id.nav_slideshow -> {}
+            R.id.nav_manage -> {}
+            R.id.nav_share -> {}
+            R.id.nav_send -> {}
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
