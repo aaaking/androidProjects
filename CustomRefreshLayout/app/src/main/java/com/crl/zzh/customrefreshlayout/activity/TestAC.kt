bb1aa0751cf37f4ca447144dcc45587548ca4637
zhouzhihui <zhouzhihui@htgames.cn>
2017-11-12 22:15:56 +0800

    some knowledge about Outline

--- CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/activity/TestAC.kt
+++ CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/activity/TestAC.kt
@@ -3,6 +3,8 @@ package com.crl.zzh.customrefreshlayout.activity
 import android.animation.ObjectAnimator
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Outline
+import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
@@ -12,6 +14,9 @@ import com.crl.zzh.customrefreshlayout.BaseActivity
 import com.crl.zzh.customrefreshlayout.R
 import com.crl.zzh.customrefreshlayout.util.ScreenUtil
 import kotlinx.android.synthetic.main.toolbar.*
+import android.view.ViewOutlineProvider
+import kotlinx.android.synthetic.main.ac_test.*
+
 
 class TestAC : BaseActivity() {
     companion object {
@@ -27,6 +32,16 @@ class TestAC : BaseActivity() {
         setSupportActionBar(toolbar)
         toolbar.setBackgroundColor(resources.getColor(R.color.red_normal))
         testSwipeback()
+        viewD.setClipToOutline(true)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            val viewOutlineProvider = object : ViewOutlineProvider() {
+//                override fun getOutline(view: View, outline: Outline) {
+//                    // 可以指定圆形，矩形，圆角矩形，path
+//                    outline.setOval(0, 0, view.width, view.height)
+//                }
+//            }
+//            viewD.setOutlineProvider(viewOutlineProvider)
+//        }
     }
 
     lateinit var decorView: View
@@ -63,10 +78,19 @@ class TestAC : BaseActivity() {
     }
 
     fun onClick(view: View) {
-        Toast.makeText(this, "${view.z}  ${view.translationZ}", Toast.LENGTH_SHORT).show()
-        val zAnim = ObjectAnimator.ofFloat(view, "translationZ", 0f, 265f)
-        zAnim.setInterpolator(OvershootInterpolator())
-        zAnim.setDuration(2000)
-        zAnim.start()
+//        Toast.makeText(this, "${view.z}  ${view.translationZ}", Toast.LENGTH_SHORT).show()
+//        val zAnim = ObjectAnimator.ofFloat(view, "translationZ", 0f, 265f)
+//        zAnim.setInterpolator(OvershootInterpolator())
+//        zAnim.setDuration(2000)
+//        zAnim.start()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val viewOutlineProvider = object : ViewOutlineProvider() {
+                override fun getOutline(view: View, outline: Outline) {
+                    // 可以指定圆形，矩形，圆角矩形，path
+                    outline.setOval(0, 0, view.width, view.height)
+                }
+            }
+            view.setOutlineProvider(viewOutlineProvider)
+        }
     }
 }
