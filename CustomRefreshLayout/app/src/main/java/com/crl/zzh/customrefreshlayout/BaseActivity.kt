ea1c6b019cd2ac28728142658ddb0a1822fc64c7
zhouzhihui <zhouzhihui@htgames.cn>
2018-01-29 10:26:13 +0800

    add vectordrawable support

--- CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/BaseActivity.kt
+++ CustomRefreshLayout/app/src/main/java/com/crl/zzh/customrefreshlayout/BaseActivity.kt
@@ -3,10 +3,16 @@ package com.crl.zzh.customrefreshlayout
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
 import android.view.WindowManager
 import com.crl.zzh.customrefreshlayout.util.ScreenUtil
 
 open class BaseActivity : AppCompatActivity() {
+    companion object {
+        init {
+            AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         ScreenUtil.init(this)
         super.onCreate(savedInstanceState)
