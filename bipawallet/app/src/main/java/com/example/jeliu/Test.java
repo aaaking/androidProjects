cdbb4b9119f50aa2513ded62399020d2d69b35c9
周智慧 <zhouzhihui@dobest.com>
2018-12-20 15:34:29 +0800

    Test.java

--- bipawallet/app/src/main/java/com/example/jeliu/Test.java
+++ bipawallet/app/src/main/java/com/example/jeliu/Test.java
@@ -1,14 +1,24 @@
 //import com.example.jeliu.bipawallet.Model.HZToken;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Test {
     public static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    public static int GG = 0;
 
     public static void main(String args[]) {
         List<HZWallet> walletList = new ArrayList<>();
@@ -27,7 +37,7 @@ public class Test {
         HashSet fs = new HashSet();
         fs.add(a);
         fs.add(b);
-        System.out.println(fs.toArray()[0].toString());
+        System.out.println("test set size:" + fs.size() + "\n" + fs.toArray()[0].toString());
         //
         String fff = "fs@";
         String[] aaa = fff.split("@");
@@ -42,6 +52,29 @@ public class Test {
             System.out.println(Arrays.toString(fsf));
             System.out.println(value);
         }
+
+        HZWallet fa = new HZWallet("a", "aN");
+        HZWallet fb = fa;//new HZWallet("b", "bN");
+        fb = fa;
+        fb.name = "faf";
+        fa = null;
+        System.out.println(fb.toString() + fa + "");
+
+        ArrayList<String> fffs = new ArrayList<>();
+        ((ArrayList<String>) fffs.clone()).add("fs");
+        System.out.println(fffs.toString());
+    }
+
+    public void sayHello(Human huamn) {
+        System.out.println("Human");
+    }
+
+    public void sayHello(Man huamn) {
+        System.out.println("Man");
+    }
+
+    public void sayHello(Woman huamn) {
+        System.out.println("Woman");
     }
 
     public static void testSys(String value) {
@@ -90,6 +123,13 @@ class HZWallet {
     @Override
     public boolean equals(Object o) {
         return this == o || o instanceof HZWallet && address != null && address.equals(((HZWallet) o).address);
+//        if (this == o) {
+//            return true;
+//        }
+//        if (!(o instanceof HZWallet)) {
+//            return false;
+//        }
+//        return address != null && name != null && address.equals(((HZWallet) o).address) && name.equals(((HZWallet) o).name);
     }
 
     @Override
@@ -100,5 +140,23 @@ class HZWallet {
     @Override
     public String toString() {
         return "address: " + address + " name: " + name;
+    }
+}
+
+class Human {
+    public void sayHello() {
+        System.out.println("Human");
+    }
+}
+
+class Man extends Human {
+    public void sayHello() {
+        System.out.println("Man");
+    }
+}
+
+class Woman extends Human {
+    public void sayHello() {
+        System.out.println("Woman");
     }
 }
